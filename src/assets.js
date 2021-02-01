a7ebf1cb8ac51421aba5046facbd0a0e1498cc86
Brian Peiris <brianpeiris@gmail.com>
2021-02-01 18:07:33 -0500

    Add new assets

--- src/assets.js
+++ src/assets.js
@@ -1,101 +1,302 @@
 export default {
-  hairStyle: [
-    { value: "hair_fauxhawk1", displayName: "Fauxhawk 1" },
-    { value: "hair_male_spikey01", displayName: "Spikey" },
-    { value: null, displayName: "none" },
+  "Earring": [
+    {
+      "value": null,
+      "displayName": "none"
+    },
+    {
+      "value": "accessory_earring_leftHigh01-silver",
+      "displayName": "LeftHigh01-silver"
+    }
   ],
-  earrings: [
+  "Sunglasses": [
     {
-      value: null,
-      displayName: "none",
+      "value": null,
+      "displayName": "none"
     },
     {
-      value: "accessory_earring_leftHigh01-silver",
-      displayName: "left high silver",
+      "value": "accessory_sunglasses_arnette",
+      "displayName": "Arnette"
     },
+    {
+      "value": "accessory_sunglasses_punk02",
+      "displayName": "Punk02"
+    }
   ],
-  sunglasses: [
+  "Eyebrows": [
     {
-      value: null,
-      displayName: "none",
+      "value": null,
+      "displayName": "none"
     },
     {
-      value: "accessory_sunglasses_arnette",
-      displayName: "Arnette",
-    },
-    {
-      value: "accessory_sunglasses_punk02",
-      displayName: "Punk",
-    },
+      "value": "character_eyebrows_brown",
+      "displayName": "Brown"
+    }
   ],
-  eyebrows: [
+  "Eyes": [
     {
-      value: "character_eyebrows-brown",
-      displayName: "Brown",
+      "value": null,
+      "displayName": "none"
     },
     {
-      value: null,
-      displayName: "none",
+      "value": "character_eyes_male01-blue",
+      "displayName": "Male01-blue"
     },
+    {
+      "value": "character_eyes_male01-brown",
+      "displayName": "Male01-brown"
+    }
   ],
-  headNeckEar: [
+  "Hands": [
     {
-      value: "character_headNeckEars_skin3",
-      displayName: "skin 3",
+      "value": null,
+      "displayName": "none"
     },
     {
-      value: null,
-      displayName: "none",
+      "value": "character_hands_skin1",
+      "displayName": "Skin1"
     },
+    {
+      "value": "character_hands_skin2",
+      "displayName": "Skin2"
+    },
+    {
+      "value": "character_hands_skin3",
+      "displayName": "Skin3"
+    },
+    {
+      "value": "character_hands_skin4",
+      "displayName": "Skin4"
+    },
+    {
+      "value": "character_hands_skin5",
+      "displayName": "Skin5"
+    },
+    {
+      "value": "character_hands_skin6",
+      "displayName": "Skin6"
+    },
+    {
+      "value": "character_hands_skin7",
+      "displayName": "Skin7"
+    },
+    {
+      "value": "character_hands_skin8",
+      "displayName": "Skin8"
+    },
+    {
+      "value": "character_hands_skinA",
+      "displayName": "SkinA"
+    },
+    {
+      "value": "character_hands_skinB",
+      "displayName": "SkinB"
+    },
+    {
+      "value": "character_hands_skinC",
+      "displayName": "SkinC"
+    },
+    {
+      "value": "character_hands_skinD",
+      "displayName": "SkinD"
+    },
+    {
+      "value": "character_hands_skinE",
+      "displayName": "SkinE"
+    },
+    {
+      "value": "character_hands_skinF",
+      "displayName": "SkinF"
+    },
+    {
+      "value": "character_hands_skinG",
+      "displayName": "SkinG"
+    },
+    {
+      "value": "character_hands_skinH",
+      "displayName": "SkinH"
+    }
   ],
-  eyes: [
+  "HeadNeckEars": [
     {
-      value: "character_eyes_male01-blue",
-      displayName: "blue",
+      "value": null,
+      "displayName": "none"
     },
     {
-      value: "character_eyes_male01-brown",
-      displayName: "brown",
+      "value": "character_headNeckEars_skin1",
+      "displayName": "Skin1"
     },
+    {
+      "value": "character_headNeckEars_skin2",
+      "displayName": "Skin2"
+    },
+    {
+      "value": "character_headNeckEars_skin3",
+      "displayName": "Skin3"
+    },
+    {
+      "value": "character_headNeckEars_skin4",
+      "displayName": "Skin4"
+    },
+    {
+      "value": "character_headNeckEars_skin5",
+      "displayName": "Skin5"
+    },
+    {
+      "value": "character_headNeckEars_skin6",
+      "displayName": "Skin6"
+    },
+    {
+      "value": "character_headNeckEars_skin7",
+      "displayName": "Skin7"
+    },
+    {
+      "value": "character_headNeckEars_skin8",
+      "displayName": "Skin8"
+    },
+    {
+      "value": "character_headNeckEars_skinA",
+      "displayName": "SkinA"
+    },
+    {
+      "value": "character_headNeckEars_skinB",
+      "displayName": "SkinB"
+    },
+    {
+      "value": "character_headNeckEars_skinC",
+      "displayName": "SkinC"
+    },
+    {
+      "value": "character_headNeckEars_skinD",
+      "displayName": "SkinD"
+    },
+    {
+      "value": "character_headNeckEars_skinE",
+      "displayName": "SkinE"
+    },
+    {
+      "value": "character_headNeckEars_skinF",
+      "displayName": "SkinF"
+    },
+    {
+      "value": "character_headNeckEars_skinG",
+      "displayName": "SkinG"
+    },
+    {
+      "value": "character_headNeckEars_skinH",
+      "displayName": "SkinH"
+    }
   ],
-  mouth: [
+  "Mouth": [
     {
-      value: "character_mouth_smirk_skin3",
-      displayName: "smirk",
+      "value": null,
+      "displayName": "none"
     },
     {
-      value: null,
-      displayName: "none",
+      "value": "character_mouth_smirk_skin1",
+      "displayName": "Smirk skin1"
     },
+    {
+      "value": "character_mouth_smirk_skin2",
+      "displayName": "Smirk skin2"
+    },
+    {
+      "value": "character_mouth_smirk_skin3",
+      "displayName": "Smirk skin3"
+    },
+    {
+      "value": "character_mouth_smirk_skin4",
+      "displayName": "Smirk skin4"
+    },
+    {
+      "value": "character_mouth_smirk_skin5",
+      "displayName": "Smirk skin5"
+    },
+    {
+      "value": "character_mouth_smirk_skin6",
+      "displayName": "Smirk skin6"
+    },
+    {
+      "value": "character_mouth_smirk_skin7",
+      "displayName": "Smirk skin7"
+    },
+    {
+      "value": "character_mouth_smirk_skin8",
+      "displayName": "Smirk skin8"
+    },
+    {
+      "value": "character_mouth_smirk_skinA",
+      "displayName": "Smirk skinA"
+    },
+    {
+      "value": "character_mouth_smirk_skinB",
+      "displayName": "Smirk skinB"
+    },
+    {
+      "value": "character_mouth_smirk_skinC",
+      "displayName": "Smirk skinC"
+    },
+    {
+      "value": "character_mouth_smirk_skinD",
+      "displayName": "Smirk skinD"
+    },
+    {
+      "value": "character_mouth_smirk_skinE",
+      "displayName": "Smirk skinE"
+    },
+    {
+      "value": "character_mouth_smirk_skinF",
+      "displayName": "Smirk skinF"
+    },
+    {
+      "value": "character_mouth_smirk_skinG",
+      "displayName": "Smirk skinG"
+    },
+    {
+      "value": "character_mouth_smirk_skinH",
+      "displayName": "Smirk skinH"
+    }
   ],
-  facialHair: [
+  "FacialHair": [
     {
-      value: null,
-      displayName: "none",
+      "value": null,
+      "displayName": "none"
     },
     {
-      value: "facialHair_goatee05",
-      displayName: "goatee",
-    },
+      "value": "facialHair_goatee05",
+      "displayName": "Goatee05"
+    }
   ],
-  torso: [
+  "Hair": [
     {
-      value: "torso_shirt_male_sportCoat01",
-      displayName: "sport coat",
+      "value": null,
+      "displayName": "none"
     },
     {
-      value: "torso_shirt_male_tshirt-moz",
-      displayName: "tshirt moz",
+      "value": "hair_fauxhawk1",
+      "displayName": "Fauxhawk1"
     },
     {
-      value: "torso_shirt_male_tshirt-unionJack",
-      displayName: "union jack",
-    },
+      "value": "hair_male_spikey01",
+      "displayName": "Male spikey01"
+    }
   ],
-  hands: [
+  "Torso": [
     {
-      value: "character_hands_skin3",
-      displayName: "skin 3",
+      "value": null,
+      "displayName": "none"
     },
-  ],
+    {
+      "value": "torso_shirt_male_sportCoat01",
+      "displayName": "Shirt male sportCoat01"
+    },
+    {
+      "value": "torso_shirt_male_tshirt-moz",
+      "displayName": "Shirt male tshirt-moz"
+    },
+    {
+      "value": "torso_shirt_male_tshirt-unionJack",
+      "displayName": "Shirt male tshirt-unionJack"
+    }
+  ]
 };
