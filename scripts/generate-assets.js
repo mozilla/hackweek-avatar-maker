77ab30df166b190814d49e75d2c4f5f30032e0b2
Brian Peiris <brianpeiris@gmail.com>
2021-02-01 18:07:06 -0500

    Add script to generate assets structure

---
+++ scripts/generate-assets.js
@@ -0,0 +1,42 @@
+const fs = require("fs");
+const path = require("path");
+
+function generateAssetsStructure(directory) { 
+  const assetFileNames = fs.readdirSync(path.resolve(directory));
+
+  const assets = {};
+
+  for (const fullname of assetFileNames) {
+    const filename = fullname.substring(0, fullname.lastIndexOf("."));
+
+    let cleanName = filename;
+
+    // Some of the GLBs have extra prefixes that we don't care about.
+    if (["character_", "accessory_"].some((prefix) => filename.startsWith(prefix))) {
+      cleanName = filename.substring(filename.indexOf("_") + 1);
+    }
+
+    let [category, ...nameParts] = cleanName.split("_");
+    category = capitalize(category);
+
+    if (!assets[category]) {
+      assets[category] = [{ value: null, displayName: "none" }];
+    }
+
+    assets[category].push({
+      value: filename,
+      displayName: capitalize(nameParts.join(" ")),
+    });
+  }
+
+  return assets;
+}
+
+const assets = generateAssetsStructure("./assets");
+const result = `export default ${JSON.stringify(assets, null, 2)};`;
+
+fs.writeFileSync("./src/assets.js", result);
+
+function capitalize(str) {
+  return str[0].toUpperCase() + str.substring(1);
+}
