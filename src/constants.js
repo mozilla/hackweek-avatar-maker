2f7ca0d2f14d2eefb7d3ec4209529af87419b791
John Shaughnessy <johnfshaughnessy@gmail.com>
2021-02-01 21:21:03 -0800

    Add controls snapshots

--- src/constants.js
+++ src/constants.js
@@ -2,4 +2,8 @@ export default {
   avatarConfigChanged: "avatarConfigChanged",
   exportAvatar: "exportAvatar",
   resetView: "resetView",
+  saveView: "saveView",
+  deleteView: "deleteView",
+  goNextView: "goNextView",
+  goPreviousView: "goPreviousView",
 };
