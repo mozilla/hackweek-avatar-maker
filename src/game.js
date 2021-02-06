890a5acb1e535ce039661f612f751bcc0009c117
Brian Peiris <brianpeiris@gmail.com>
2021-02-06 05:26:51 -0500

    cleanup

--- src/game.js
+++ src/game.js
@@ -89,32 +89,32 @@ function resetView() {
 
 function createSkydome(radius) {
   const vertexShader = `
-varying vec3 vWorldPosition;
+    varying vec3 vWorldPosition;
 
-void main() {
+    void main() {
 
-  vec4 worldPosition = modelMatrix * vec4( position, 1.0 );
-  vWorldPosition = worldPosition.xyz;
+      vec4 worldPosition = modelMatrix * vec4( position, 1.0 );
+      vWorldPosition = worldPosition.xyz;
 
-  gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+      gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
 
-}
-`;
+    }
+  `;
   const fragmentShader = `
-uniform vec3 topColor;
-uniform vec3 bottomColor;
-uniform float offset;
-uniform float exponent;
+    uniform vec3 topColor;
+    uniform vec3 bottomColor;
+    uniform float offset;
+    uniform float exponent;
 
-varying vec3 vWorldPosition;
+    varying vec3 vWorldPosition;
 
-void main() {
+    void main() {
 
-  float h = normalize( vWorldPosition + offset ).y;
-  gl_FragColor = vec4( mix( bottomColor, topColor, max( pow( max( h , 0.0), exponent ), 0.0 ) ), 1.0 );
+      float h = normalize( vWorldPosition + offset ).y;
+      gl_FragColor = vec4( mix( bottomColor, topColor, max( pow( max( h , 0.0), exponent ), 0.0 ) ), 1.0 );
 
-}
-`;
+    }
+  `;
 
   const offset = radius / 12;
 
