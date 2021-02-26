83ef94f773b27f8686bb857a97045249899834fb
Brian Peiris <brianpeiris@gmail.com>
2021-02-25 19:58:35 -0500

    Add more menu and link to github

---
+++ src/react-components/MoreMenu.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+export function MoreMenu({ items }) {
+  const [menuVisible, setMenuVisible] = useState(false);
+  return (
+    <div className="menuContainer">
+      <button className="menuButton" onClick={() => setMenuVisible(!menuVisible)}>···</button>
+    {menuVisible && <div className="menu">{items}</div>}
+    </div>
+  );
+}
