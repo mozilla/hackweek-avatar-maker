5638ee32382b2ed231f4a04dc2215d0db1a7c529
Matthew Nichols <matthew@matthew-nichols.com>
2021-03-05 10:03:57 +0100

    Created local storage persistance for avatars

---
+++ src/persistence.js
@@ -0,0 +1,60 @@
+const storageKey = "avatarMaker";
+
+function getConfigFromLocalStorage() {
+    const stored = localStorage.getItem(storageKey);
+    if (stored) {
+        return JSON.parse(stored);
+    } else {
+        return { avatars: {} };
+    }
+}
+
+function persistToLocalStorage(config) {
+    localStorage.setItem(storageKey, JSON.stringify(config))
+}
+
+/**
+ * Saves an Avatar config object
+ * @param {string} slotKey key to store under
+ * @param {object} avatarConfig Avatar config object to store
+ */
+export function saveAvatarConfig(slotKey, avatarConfig) {
+    if (!window.localStorage) {
+        console.log("No localStorage...bailing.");
+    }
+
+    const storedConfig = getConfigFromLocalStorage();
+
+    storedConfig.avatars[slotKey] = avatarConfig;
+
+    persistToLocalStorage(storedConfig);
+}
+
+/**
+ * Returns the specified Avatar config object
+ * @param {string} slotKey The key for the desired Avatar config
+ * @returns {object} Avatar config
+ */
+export function getAvatarConfig(slotKey) {
+    if (!window.localStorage) {
+        console.log("No localStorage...bailing.");
+    }
+
+    const storedConfig = getConfigFromLocalStorage();
+
+    return storedConfig.avatars[slotKey];
+}
+
+/**
+ * Returns list of the Avatar slotKeys
+ * @returns {Array} List of Avatar slotKeys that have stored data.
+ */
+export function getAvatarConfigSlotKeys() {
+    if (!window.localStorage) {
+        console.log("No localStorage...bailing.");
+    }
+
+    const storedConfig = getConfigFromLocalStorage();
+
+    return Object.keys(storedConfig.avatars);
+}
